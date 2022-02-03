ed1dee015d000684671a2eeb4ded7fcc4d7914b9
Jeormy0515 <qkr07224@gmail.com>
2022-02-04 00:42:38 +0900

    Update view and datasource

--- src/main/java/com/norticeboard/smc/controller/PostController.java
+++ src/main/java/com/norticeboard/smc/controller/PostController.java
@@ -5,20 +5,22 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import com.norticeboard.smc.model.dto.UserDTO;
+import com.norticeboard.smc.mybatis.mapper.UserMapper;
 import com.norticeboard.smc.service.UserService;
 
 @Controller
 public class PostController {
 	
 	@Autowired
-	UserService userService;
+	UserMapper userMapper;
+	
 	
 	@PostMapping("/signup")
 	public String signup(@ModelAttribute("user") UserDTO userDTO) {
 		System.out.println("userName : " + userDTO.getUserName());
 		System.out.println("userId : " + userDTO.getUserId());
 		System.out.println("userPassword : " + userDTO.getUserPassword());
-		userService.insertUser(userDTO);
+		userMapper.insertUser(userDTO);
 		return "redirect:/login";
 	}
 	
